08a7a1d084a43fac38d1bdc1c33142d534b690fe
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2025-08-04 14:30:22 +0200

    lesson 50

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,19 +1,21 @@
-import { Component, input } from '@angular/core';
+import {Component, input, signal} from '@angular/core';
 import { Task } from './task/task';
-import { DUMMY_TASKS } from '../dummy-tasks';
+import {DUMMY_TASKS, TaskElement} from '../dummy-tasks';
+import {NewTask} from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss'
 })
 export class Tasks {
   name = input<string | undefined>();
+  selectedUserId = input.required<string | undefined>();
 
+  isAddingTask = signal<boolean>(false);
   tasks = DUMMY_TASKS;
 
-  selectedUserId = input.required<string | undefined>();
 
   getUserTasks() {
     return this.tasks.filter(task => task.userId === this.selectedUserId());
@@ -21,5 +23,9 @@ export class Tasks {
 
   onCompleteTask(id:string) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask.set(true);
   }
 }
